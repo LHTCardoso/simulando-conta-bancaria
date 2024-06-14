5ce245394b19400365c0a109cd23c91a1aa08ba2
LHTCardoso <cardosinhoo15@gmail.com>
2024-06-14 10:44:02 -0300

    Atualização

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,3 @@
-import java.util.Locale;
-import java.util.Scanner;
-
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // TODO: conhecer e importar a classe Scanner
